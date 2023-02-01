58e6da6e9ab4f336ac010951b3a7e723750af814
Charis2424 <xaros_24@hotmail.com>
2023-02-01 20:50:50 +0200

    -Fixed Navbar (LogIn/LogOut)

--- frontend/user-interface/src/components/AdminUI.js
+++ frontend/user-interface/src/components/AdminUI.js
@@ -1,21 +1,12 @@
-﻿import { useNavigate, Link } from 'react-router-dom';
-import useLogout from "../hooks/useLogout"
-import QuestionsList from './QuestionsList';
+﻿import QuestionsList from './QuestionsList';
 
 function AdminUI() {
-    const navigate = useNavigate();
-    const logout = useLogout();
 
-    const signOut = async() => {
-        await logout();
-        navigate('/');
-    }
 
     return (
         <div>
             <h2>Logged in as Admin</h2>
             <QuestionsList />
-            <button onClick={signOut}>Sign Out</button>
         </div>);
 };
 
--- frontend/user-interface/src/components/NavBar.js
+++ frontend/user-interface/src/components/NavBar.js
@@ -1,18 +1,49 @@
 ﻿import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from "react-router-dom";
+import useLogout from "../hooks/useLogout"
+import useRefreshToken from '../hooks/useRefreshToken';
+import useAuth from '../hooks/useAuth';
 
 function NavBar() {
+
+    /*    const { loginWithRedirect, isAuthenticated } = useAuth0();*/
+    const logout = useLogout();
+    const { auth } = useAuth();
+
     return (
-        <div>
-            <nav className="navbar navbar-expand-sm navbar-toggleable-sm navbar-dark bg-dark border-bottom box-shadow mb-3">
-                <div className="container-fluid">
-                    <a className="navbar-brand" >Assignment_4A_Team2556</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="navbar-collapse collapse d-sm-inline-flex justify-content-between">
-                        <ul className="navbar-nav flex-grow-1">
+
+        <>
+            {/*<nav className="navbar navbar-expand-sm navbar-toggleable-sm navbar-dark bg-dark border-bottom box-shadow mb-3">*/}
+            {/*    <div className="container-fluid">*/}
+            {/*        <a className="navbar-brand" >Assignment_4A_Team2556</a>*/}
+            {/*        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent"*/}
+            {/*            aria-expanded="false" aria-label="Toggle navigation">*/}
+            {/*            <span className="navbar-toggler-icon"></span>*/}
+            {/*        </button>*/}
+            {/*        <div className="navbar-collapse collapse d-sm-inline-flex justify-content-between">*/}
+            {/*            <ul className="navbar-nav flex-grow-1">*/}
+            {/*                <li className="nav-item">*/}
+            {/*                    <Link className="nav-link text-light" to="Home" >Home</Link>*/}
+            {/*                </li>*/}
+            {/*                <li className="nav-item">*/}
+            {/*                    <Link className="nav-link text-light" to="AdminUI" >Admin UI</Link>*/}
+            {/*                </li>*/}
+            {/*                <li className="nav-item">*/}
+            {/*                    <a className="nav-link text-light">Candidate UI</a>*/}
+            {/*                </li>*/}
+            {/*                <li className="nav-item">*/}
+            {/*                    <Link className="nav-link text-light" to="Register" >Register</Link>*/}
+            {/*                </li>*/}
+            {/*                <li className="nav-item">*/}
+            {/*                    <Link className="nav-link text-light" to="Login" >Login</Link>*/}
+            {/*                </li>*/}
+            {/*            </ul>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</nav>*/}
+            <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+                <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+                    <ul class="navbar-nav me-auto">
                             <li className="nav-item">
                                 <Link className="nav-link text-light" to="Home" >Home</Link>
                             </li>
@@ -22,17 +53,37 @@ function NavBar() {
                             <li className="nav-item">
                                 <a className="nav-link text-light">Candidate UI</a>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link text-light" to="Register" >Register</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link text-light" to="Login" >Login</Link>
-                            </li>
-                        </ul>
-                    </div>
+                    </ul>
                 </div>
+                <div class="mx-auto order-0">
+                    <a class="navbar-brand mx-auto">Assignment_4A_Team2556</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".dual-collapse2">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+                {auth?.accessToken?  (
+                    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+                      
+                        <button class="nav-link ms-auto btn btn-dark text-light" type="button" onClick={() => logout()} >
+                            Sign Out
+                        </button>
+                    </div>
+                ) : (
+                     
+                            <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+                                <ul class="navbar-nav ms-auto">
+                                    <li class="nav-item">
+                                        <Link className="nav-link text-light" to="Register" >Register</Link>
+                                    </li>
+                                    <li class="nav-item">
+                                        <Link className="nav-link text-light" to="Login" >Login</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        
+                        )}
             </nav>
-        </div>
+        </>
 
     );
 }
