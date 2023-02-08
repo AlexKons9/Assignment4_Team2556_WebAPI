209bd0f0cc92c0c6198123ba6700c5e214977de1
alkrec <118450684+alkrec@users.noreply.github.com>
2023-02-08 17:27:28 +0200

    still debugging...

--- frontend/user-interface/src/components/Exams/SchedulerMenu.js
+++ frontend/user-interface/src/components/Exams/SchedulerMenu.js
@@ -1,68 +1,66 @@
-import useAuth from "../hooks/useAuth";
-import useAxiosPrivate from "../hooks/useAxiosPrivate";
+import useAuth from "../../hooks/useAuth";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import React, { useState, useEffect, useNavigate } from "react";
+
 
 function SchedulerMenu() {
     const { auth } = useAuth();
     const userName = auth.userName;
     const axiosPrivate = useAxiosPrivate();
+    //const navigate = useNavigate();
 
-    const [examDetailsDTO, setExamDetailsDTO] = useState(
-        {
-            certificateId: "",
-            userName: "",
-            examDate: ""
+    const [examDate, setExamDate] = useState();
+    const [voucherDescription, setVoucherDescription] = useState();
+
+    const onChangeHandlerDate = (event) => {
+        const { name, value } = event.target;
+        setExamDate({ ...examDate, [name]: value });
+    };
+    const onChangeHandlerVoucher = (event) => {
+        const { name, value } = event.target;
+        setVoucherDescription({ ...voucherDescription, [name]: value });
+    };
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            console.log(voucherDescription.voucher);
+
+            const answer = await axiosPrivate.get(`/api/Vouchers/GetVoucher/${voucherDescription.voucher}`);
+            const voucher = answer.data;
+            console.log(JSON.stringify(voucher));
+            const response = await axiosPrivate.post(`/api/CandidateExams/InsertVoucher/${userName}/examDate=${examDate.date}`, voucher);
+            console.log(response.data);
+
+            const candidateExamId = response.data.candidateExamId;
+            const questionsList = response.data.questions;
+
+            voucher.isClaimed = true;
+            await axiosPrivate.put(`/api/Vouchers/${voucher.voucherId}`, voucher);
+
+            //Navigate to exams scheduled for later list
+            // navigate("/CandidateUI/");
+        } catch (error) {
+            console.error(error);
+            alert("Error creating Exam");
         }
-    );
-
-    // const onChangeHandler = (event) => {
-    //     const { name, value } = event.target;
-    //     setExamDetailsDTO({ ...examDetailsDTO, [name]: value });
-    //   };
+    };
 
 
-    // const handleSubmit = async (event) => {
-    //     event.preventDefault();
-    //     try {
-    //       //console.log(voucherDescription.voucher);
-    //       const answer = await axiosPrivate.get(`/api/Vouchers/GetVoucher/${voucherDescription.voucher}`);
-    //       const voucher = answer.data;
-    //       //console.log(JSON.stringify(voucher));
-    //       const response = await axiosPrivate.post(`/api/CandidateExams/InsertVoucher/${userName}`, voucher);
-    //       // console.log(response.data);
-    
-    //       const candidateExamId = response.data.candidateExamId;
-    //       const questionsList = response.data.questions;
-    
-    //       voucher.isClaimed = true;
-    //       await axiosPrivate.put(`/api/Vouchers/${voucher.voucherId}`, voucher);
-          
-          
-    //       navigate("/CandidateUI/GenerateExam", {
-    //         state: {
-    //           candidateExamId: candidateExamId,
-    //           questionList: questionsList,
-    //         },
-    //       });
-    //     } catch (error) {
-    //       console.error(error);
-    //       alert("Error creating Exam");
-    //     }
-    //   };
-
-
-    return(
+    return (
         <div className="container">
             <h1>Exam Scheduler Menu</h1>
             <form onSubmit={handleSubmit}>
 
                 <div className="form-group ">
                     <label>Insert Voucher Code</label>
-                    <input onChange={onChangeHandler} type="text" id="voucher" name="voucher"></input>
+                    <input onChange={onChangeHandlerVoucher} type="text" id="voucher" name="voucher"></input>
                 </div>
 
                 <div className="form-group ">
                     <label>Select Date</label>
-                    <input type="date" id="date" name="date"></input>
+                    <input onChange={onChangeHandlerDate} type="date" id="date" name="date"></input>
                 </div>
 
                 <button type="submit" className="btn btn-primary">
