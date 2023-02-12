b0920860d459dd9a76b70cbe65c398b55fbc8ad2
AlexKons9 <alepe93@yahoo.com>
2023-02-12 16:24:40 +0200

    Frontend changed Lepe

--- frontend/user-interface/src/components/CandidateExams/GenerateExam.js
+++ frontend/user-interface/src/components/CandidateExams/GenerateExam.js
@@ -1,27 +1,27 @@
 ﻿import React, { useState } from "react";
-import axios from "axios";
+import "./GenerateExam.css";
 import { useLocation, useNavigate } from "react-router-dom";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import SuccessModal from "../Modals/SuccessModal";
 
-function GenerateExam()
-{
+function GenerateExam() {
     const location = useLocation();
     // function that navigate to another js view (used in handleSubmit function)
     const navigate = useNavigate();
+    const arrayABCD = ['A', 'B', 'C', 'D'];
     const [modalSuccess, setModalSuccess] = useState(false);
     const [examForm, setExamForm] = useState({
         candidateExamId: location.state.candidateExamId,
         questions: location.state.questionList,
-        chosenOptionsId:[]
+        chosenOptionsId: []
     });
     const axiosPrivate = useAxiosPrivate();
-    
-    const handleOptionSelection  = (i, optionId) => {
+
+    const handleOptionSelection = (i, optionId) => {
         //const { name, value } = event.target;
         const updatedChosenOptionsId = [...examForm.chosenOptionsId];
         updatedChosenOptionsId[i] = optionId;
-        setExamForm({ ...examForm, chosenOptionsId: updatedChosenOptionsId  });
+        setExamForm({ ...examForm, chosenOptionsId: updatedChosenOptionsId });
     };
 
     const showModalHandler = () => {
@@ -31,13 +31,13 @@ function GenerateExam()
     const closeModalHandler = () => {
         setModalSuccess(false);
     }
-    
+
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        try {      
+        try {
             //await axios.post("https://localhost:7015/api/CandidateExams",examForm);
-            await axiosPrivate.post("/api/CandidateExams",examForm)
+            await axiosPrivate.post("/api/CandidateExams", examForm)
             showModalHandler();
             //const response = await axios.get(`https://localhost:7015/api/CandidateExams/ExamResults/${examForm.candidateExamId}`)
             const response = await axiosPrivate.get(`/api/CandidateExams/ExamResults/${examForm.candidateExamId}`)
@@ -53,31 +53,52 @@ function GenerateExam()
     return (
         <>
             <SuccessModal
-            show={modalSuccess}
-            body="Your answers submitted correctly!"
-            closeModalHandler={closeModalHandler}
+                show={modalSuccess}
+                body="Your answers submitted correctly!"
+                closeModalHandler={closeModalHandler}
             ></SuccessModal>
             <h2>Exam Time</h2>
-            <form onSubmit={handleSubmit}>
-                <input type="hidden" name="candidateExamId" value={examForm.candidateExamId} />
-                {examForm.questions.map((question, i) => (
-                    <div key={question.questionId}>
-                        <p>{question.descriptionStem}</p>
-                        {question.options.map((option, j) => (
-                            <div key={option.optionId}>
-                                <input
-                                    type="radio"
-                                    name={`chosenOptionsId[${i}]`}
-                                    value={option.optionId}
-                                    onChange={() => handleOptionSelection(i, option.optionId)} //handleOptionSelection
-                                />
-                                {option.description}
+            <div className="container">
+                <form onSubmit={handleSubmit} >
+                    <div className="mt-4 ">
+                        <input type="hidden" name="candidateExamId" value={examForm.candidateExamId} />
+                        {examForm.questions.map((question, i) => (
+
+                            <div className="mt-5"  key={question.questionId}>
+                                <h5 id="questionDesc">{i + 1}. {question.descriptionStem}</h5>
+
+
+                                <hr />
+                                {question.options.map((option, j) => (
+                                    <div>
+                                        <table className="table table-striped">
+                                            <tbody>
+                                                <tr key={option.optionId} className="text-start">
+                                                    <td scope="row" >
+                                                        <input
+                                                            className="form-check-input"
+                                                            type="radio"
+                                                            name={`chosenOptionsId[${i}]`}
+                                                            value={option.optionId}
+                                                            onChange={() => handleOptionSelection(i, option.optionId)} //handleOptionSelection
+                                                            />
+                                                        {" "}
+                                                        {/* <span>&nbsp;</span> */}
+                                                        <span>{arrayABCD[j]}{") "} &nbsp;</span>
+                                                        <span>{option.description}</span>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>                                ))}
+                                    <hr />
                             </div>
                         ))}
+                        <input type="submit" value="Submit Exam" className="btn btn-primary" />
                     </div>
-                ))}
-                <input type="submit" value="Submit Exam" className="btn btn-primary" />
-            </form>
+                </form>
+
+            </div>
         </>
     );
 };
--- frontend/user-interface/src/components/Candidates/EditCandidate.js
+++ frontend/user-interface/src/components/Candidates/EditCandidate.js
@@ -37,93 +37,96 @@ function EditCandidate() {
 
     return (
         <div className="container">
-        <h2>Edit Candidate</h2>
+        <h2 className="mb-3">Edit Candidate</h2>
             <form onSubmit={handleSubmit} className="row g-3 mt-3 form-container">
-                    <div className="col-md-4">
-                        <label for="name" className="form-label">First Name:</label>
+                    <h5>Personal Details</h5>
+                    <div className="col-md-6">
+                        <label for="name" className="form-label">First Name</label>
                         <input type="text" id="name" name="firstName" className="form-control"  value={candidate.firstName}
                         onChange={handleChange}></input>
                     </div>    
-                    <div className="col-md-4">
-                        <label for="middle_name" className="form-label">Middle Name:</label>
+                    <div className="col-md-6">
+                        <label for="middle_name" className="form-label">Middle Name</label>
                         <input type="text" id="middle_name" name="middleName" className="form-control" value={candidate.middleName}
                         onChange={handleChange}></input>
                     </div>
-                    <div className="col-md-4">
-                        <label for="last_name" className="form-label">Last Name:</label>
+                    <div className="col-md-12 mb-3">
+                        <label for="last_name" className="form-label">Last Name</label>
                         <input type="text" id="last_name" name="lastName" className="form-control" value={candidate.lastName}
                         onChange={handleChange}></input>
                     </div>
-                    <div className="col-md-4">
-                        <label for="birth_date" className="form-label"> Birth Date:</label>
+                    <div className="col-md-6">
+                        <label for="birth_date" className="form-label"> Birth Date</label>
                         <input type="date" name="birthDate" id="birth_date" className="form-control"
                         value={candidate.birthDate} onChange={handleChange}></input>
                     </div>
-                    <div class="col-md-4">
-                        <label for="gender" class=" form-label px-2 fw-bold">Gender:
+                    <div class="col-md-6">
+                        <label for="gender" class=" form-label px-2 fw-bold">Gender
                         {/* <span class="text-danger">*</span> */}
                         </label>                                
                         <select class="form-select" id="gender" name="gender" value={candidate.gender} onChange={handleChange} required>                                    
-                        <option selected value="">Please Select Gender:</option>                                    
+                        <option selected value="">Please Select Gender</option>                                    
                         <option value="male">Male</option>                                    
                         <option value="female">Female</option>                                    
                         <option value="preferNotToSay">Prefer not to say</option>                                
                         </select>                            
                     </div> 
-                    <div className="col-md-4">
-                        <label for="native_language" className="form-label">Native Language:</label>
+                    <div className="col-md-6">
+                        <label for="native_language" className="form-label">Native Language</label>
                         <input type="text" id="native_language" name="nativeLanguage" className="form-control" value={candidate.nativeLanguage} onChange={handleChange}></input>
                     </div>
-                    <div class="col-md-4">                                
-                    <label for="photoIdType" class="form-label px-2  fw-bold">Photo Id Type:</label>      
+                    <div class="col-md-6">                                
+                    <label for="photoIdType" class="form-label px-2  fw-bold">Photo Id Type</label>      
                     <select class="form-select" id="photoIdType" name="photoIdType" value={candidate.photoIdType} onChange={handleChange}>  
-                    <option selected value="">Please Select ID type:</option>  
+                    <option selected value="">Please Select ID type</option>  
                     <option value="nationalId">National Id</option>                                    
                     <option value="Passport">Passport</option>                                
                     </select>                            
-                    </div>               
-                    <div className="col-md-4">
-                        <label for="id_number" className="form-label">ID Number:</label>
+                    </div>   
+                    <div className="col-md-6">
+                        <label for="id_number" className="form-label">ID Number</label>
                         <input type="text" id="id_number" name="photoIdNumber" className="form-control" value={candidate.photoIdNumber} onChange={handleChange}></input>
                     </div>
-                    <div className="col-md-4">
-                        <label for="id_date" className="form-label">ID Issue Date:</label>
+                    <div className="col-md-6 mb-5">
+                        <label for="id_date" className="form-label">ID Issue Date</label>
                         <input type="date" id="id_date" name="photoIssueDate" className="form-control" value={candidate.photoIssueDate} onChange={handleChange}></input>
                     </div>
-                    <div>
-                        <label for="email" className="form-label"> Email:</label>
+
+                    <h5 className="mt-4">Communication Details</h5>
+                    <div className="col-md-12">
+                        <label for="email" className="form-label"> Email</label>
                         <input type="email" name="email" id="email" className="form-control" value={candidate.email} onChange={handleChange}></input>
                     </div> 
-                    <div className="col-md-4">
-                        <label for="address" className="form-label"> Address:</label>
+                    <div className="col-md-6">
+                        <label for="address" className="form-label"> Address</label>
                         <input type="text" name="address" id="addressLine" className="form-control" value={candidate.addressLine} onChange={handleChange}></input>
                     </div> 
-                    <div className="col-md-4">
-                        <label for="address_number" className="form-label"> Address Number:</label>
+                    <div className="col-md-6">
+                        <label for="address_number" className="form-label"> Address Number</label>
                         <input type="text" name="addressLine2" id="address_number" className="form-control" value={candidate.addressLine2} onChange={handleChange}></input>
                     </div> 
-                    <div className="col-md-4">
-                        <label for="postal_code" className="form-label"> Postal Code:</label>
+                    <div className="col-md-6">
+                        <label for="postal_code" className="form-label"> Postal Code</label>
                         <input id="postal_code" name="postalCode" type="text" pattern="[0-9]*" className="form-control" value={candidate.postalCode} onChange={handleChange}></input>
                     </div> 
-                    <div className="col-md-4">
-                        <label for="country_residence" className="form-label"> Country of Residence:</label>
+                    <div className="col-md-6">
+                        <label for="country_residence" className="form-label"> Country of Residence</label>
                         <input type="text" name="countryOfResidence" id="country_residence" className="form-control" value={candidate.countryOfResidence} onChange={handleChange}></input>
                     </div> 
-                    <div className="col-md-4">
-                        <label for="state" className="form-label">State:</label>
+                    <div className="col-md-6">
+                        <label for="state" className="form-label">State</label>
                         <input type="text" name="province" id="state" className="form-control" value={candidate.province} onChange={handleChange}></input>
                     </div> 
-                    <div className="col-md-4">
-                        <label for="city" className="form-label"> City:</label>
+                    <div className="col-md-6">
+                        <label for="city" className="form-label"> City</label>
                         <input type="text" name="city" id="city" className="form-control" value={candidate.city} onChange={handleChange}></input>
                     </div> 
                     <div className="col-md-6">
-                        <label for="landline_number" className="form-label"> Landline Number :</label>
+                        <label for="landline_number" className="form-label"> Landline Number </label>
                         <input id="landline_number" name="landlineNumber" type="tel" className="form-control" value={candidate.landlineNumber} onChange={handleChange}></input>
                     </div> 
                     <div className="col-md-6">
-                        <label for="mobile_number" className="form-label"> Mobile Number :</label>
+                        <label for="mobile_number" className="form-label"> Mobile Number </label>
                         <input id="mobile_number" name="mobileNumber" type="tel" className="form-control" value={candidate.mobileNumber} onChange={handleChange}></input>
                     </div> 
                    
